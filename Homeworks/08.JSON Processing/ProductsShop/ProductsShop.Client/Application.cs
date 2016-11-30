aad318beb4294cd548c7a9b0f6743cfbfd4be173
onefen <ivo.malakov85@gmail.com>
2016-11-30 18:43:35 +0200

    Homework "JSON Processing" modified. "Query and export data" part for "Products Shop" Database made it (Task 3).

--- Homeworks/08.JSON Processing/ProductsShop/ProductsShop.Client/Application.cs
+++ Homeworks/08.JSON Processing/ProductsShop/ProductsShop.Client/Application.cs
@@ -13,7 +13,95 @@
         {
            ProductsShopContext context = new ProductsShopContext();
 
-           SeedDataBase(context);
+           //SeedDataBase(context);
+
+           //ProductInRange(context);
+           //SuccessfullySoldProducts(context);
+           //CategoriesByProductCount(context);
+           UsersAndProducts(context);
+        }
+
+        private static void UsersAndProducts(ProductsShopContext context)
+        {
+            var wantedUsers = context.Users
+                .Where(user => user.ProductsSold.Count >= 1)
+                .OrderByDescending(user => user.ProductsSold.Count)
+                .ThenBy(user => user.LastName)
+                .Select(user => new
+                {
+                    firstName = user.FirstName,
+                    lastName = user.LastName,
+                    age = user.Age,
+                    soldProducts = user.ProductsSold.Select(product => new
+                    {
+                        name = product.Name,
+                        price = product.Price
+                    })
+                });
+
+            string jsonObject = JsonConvert.SerializeObject(wantedUsers, Formatting.Indented);
+            //Console.WriteLine(jsonObject);
+            File.WriteAllText("../../../../results/users_and_products.json", jsonObject);
+        }
+
+        private static void CategoriesByProductCount(ProductsShopContext context)
+        {
+            var wantedCategories = context.Categories
+                .OrderBy(category => category.Products.Count)
+                .Select(category => new
+                {
+                    name = category.Name,
+                    productsCoutn = category.Products.Count,
+                    averagePrice = category.Products.Average(product => product.Price),
+                    totalRevenue = category.Products.Sum(product => product.Price)
+                });
+
+            string jsonObject = JsonConvert.SerializeObject(wantedCategories, Formatting.Indented);
+            //Console.WriteLine(jsonObject);
+            File.WriteAllText("../../../../results/category_by_products_count.json", jsonObject);
+        }
+
+        private static void SuccessfullySoldProducts(ProductsShopContext context)
+        {
+            var wantedUsers = context.Users
+                .Where(user => user.ProductsSold.Count(product => product.Buyer != null) > 0)
+                .OrderBy(user => user.LastName)
+                .ThenBy(user => user.FirstName)
+                .Select(user => new
+                {
+                    firstName = user.FirstName,
+                    lastName = user.LastName,
+                    soldProducts = user.ProductsSold
+                        .Where(product => product.Buyer.FirstName != null && product.Buyer.LastName != null)
+                        .Select(product => new
+                        {
+                            name = product.Name,
+                            price = product.Price,
+                            buyerFirstName = product.Buyer.FirstName,
+                            buyerLastName = product.Buyer.LastName
+                        })
+                });
+
+            string jsonUsers = JsonConvert.SerializeObject(wantedUsers, Formatting.Indented);
+            //Console.WriteLine(jsonUsers);
+            File.WriteAllText("../../../../results/successfully_sold_products.json", jsonUsers);
+        }
+
+        private static void ProductInRange(ProductsShopContext context)
+        {
+            var wantedProducts = context.Products
+                .Where(product => product.Price > 500 && product.Price < 1000 && product.Buyer == null)
+                .OrderBy(product => product.Price)
+                .Select(product => new
+                {
+                    product.Name,
+                    product.Price,
+                    seller = (product.Seller.FirstName + " " + product.Seller.LastName.Trim())
+                });
+
+            string jsonObject = JsonConvert.SerializeObject(wantedProducts, Formatting.Indented);
+            //Console.WriteLine(jsonObject);
+            File.WriteAllText("../../../../results/products_in_range.json", jsonObject);
         }
 
         private static void SeedDataBase(ProductsShopContext context)
